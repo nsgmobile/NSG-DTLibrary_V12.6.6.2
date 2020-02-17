687e879f20774efa9d26d65681767aedbda5ccfc
sailaja-cheela <er.sailaja.cheela@gmail.com>
2020-02-17 14:37:13 +0530

    conmmit--changes in ETA, Location,fused api--working navigation test

--- NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
+++ NsgDtLibrary/src/main/java/com/nsg/nsgdtlibrary/Classes/util/NSGIMapFragmentActivity.java
@@ -1011,66 +1011,65 @@ import static java.lang.Math.sin;
                         Log.e("returnedDistance", "RouteDiationPosition  ###### " + routeDiationPosition);
                         //   Log.e("returnedDistance", "Destination Position --------- " + destPoint);
                         //  DestinationPosition = new LatLng(destLat, destLng);
-                          dialog = new ProgressDialog(getActivity(), R.style.ProgressDialog);
-                          dialog.setMessage("Fetching new Route");
-                          dialog.setMax(100);
-                          dialog.show();
-                          handler.postDelayed(new Runnable() {
-                                public void run() {
-                                    dialog.dismiss();
-                                    String MESSAGE = "";
-                                    GetRouteDetails(routeDiationPosition, destPoint);
-                                    //checkPointsOfRoue1withNewRoute(EdgeWithoutDuplicates,PointBeforeRouteDeviation);
-                                    if (RouteDeviationConvertedPoints != null && RouteDeviationConvertedPoints.size() > 0) {
-                                        isRouteDeviated = true;
 
-                                        LayoutInflater inflater1 = getActivity().getLayoutInflater();
-                                        @SuppressLint("WrongViewCast") View layout = inflater1.inflate(R.layout.custom_toast, (ViewGroup) getActivity().findViewById(R.id.textView_toast));
-                                        TextView text = (TextView) layout.findViewById(R.id.textView_toast);
+                        new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.dismiss();
+                                String MESSAGE = "";
+                                GetRouteDetails(routeDiationPosition, destPoint);
+                                //checkPointsOfRoue1withNewRoute(EdgeWithoutDuplicates,PointBeforeRouteDeviation);
+                                if (RouteDeviationConvertedPoints != null && RouteDeviationConvertedPoints.size() > 0) {
+                                    isRouteDeviated = true;
 
-                                        text.setText("Route Deviated");
+                                    LayoutInflater inflater1 = getActivity().getLayoutInflater();
+                                    @SuppressLint("WrongViewCast") View layout = inflater1.inflate(R.layout.custom_toast, (ViewGroup) getActivity().findViewById(R.id.textView_toast));
+                                    TextView text = (TextView) layout.findViewById(R.id.textView_toast);
 
-                                        Toast toast = new Toast(getActivity().getApplicationContext());
-                                        toast.setDuration(Toast.LENGTH_LONG);
-                                        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
-                                        toast.setGravity(Gravity.TOP, 0, 150);
-                                        toast.setView(layout);
-                                        toast.show();
-                                        StringBuilder routeDeviatedAlert = new StringBuilder();
-                                        routeDeviatedAlert.append("ROUTE DEVIATED" + "RouteDeviatedSourcePosition : " + RouteDeviatedSourcePosition);
-                                        sendData(MapEvents.ALERTVALUE_3, MapEvents.ALERTTYPE_3);
-                                        if (mPositionMarker != null) {
-                                            mPositionMarker.remove();
-                                            Log.e("REMOVING MARKER", "REMOVING MARKER");
-                                        }
-                                        mPositionMarker = mMap.addMarker(new MarkerOptions()
-                                                .position(currentGpsPosition)
-                                                .title("currentLocation")
-                                                .anchor(0.5f, 0.5f)
-                                                .flat(true)
-                                                .icon(bitmapDescriptorFromVector(getContext(), R.drawable.gps_transperent)));
+                                    text.setText("Route Deviated");
 
-
-                                        CameraUpdate center =
-                                                CameraUpdateFactory.newLatLng(currentGpsPosition);
-                                        CameraUpdate zoom = CameraUpdateFactory.zoomTo(22);
-                                        mMap.moveCamera(center);
-                                        mMap.animateCamera(zoom);
-                                        if (mPositionMarker != null && mPositionMarker.isVisible() == true) {
-                                            PolylineOptions polylineOptions = new PolylineOptions();
-                                            polylineOptions.add(OldGPSPosition);
-                                            polylineOptions.addAll(RouteDeviationConvertedPoints);
-                                            Polyline polyline = mMap.addPolyline(polylineOptions);
-                                            polylineOptions.color(Color.RED).width(30);
-                                            mMap.addPolyline(polylineOptions);
-                                            polyline.setJointType(JointType.ROUND);
-                                        }
+                                    Toast toast = new Toast(getActivity().getApplicationContext());
+                                    toast.setDuration(Toast.LENGTH_LONG);
+                                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                                    toast.setGravity(Gravity.TOP, 0, 150);
+                                    toast.setView(layout);
+                                    toast.show();
+                                    StringBuilder routeDeviatedAlert = new StringBuilder();
+                                    routeDeviatedAlert.append("ROUTE DEVIATED" + "RouteDeviatedSourcePosition : " + RouteDeviatedSourcePosition);
+                                    sendData(MapEvents.ALERTVALUE_3, MapEvents.ALERTTYPE_3);
+                                    if (mPositionMarker != null) {
+                                        mPositionMarker.remove();
+                                        Log.e("REMOVING MARKER", "REMOVING MARKER");
                                     }
-                                    handler.postDelayed(this, 10);
+                                    mPositionMarker = mMap.addMarker(new MarkerOptions()
+                                            .position(currentGpsPosition)
+                                            .title("currentLocation")
+                                            .anchor(0.5f, 0.5f)
+                                            .flat(true)
+                                            .icon(bitmapDescriptorFromVector(getContext(), R.drawable.gps_transperent)));
+
+
+                                    CameraUpdate center =
+                                            CameraUpdateFactory.newLatLng(currentGpsPosition);
+                                    CameraUpdate zoom = CameraUpdateFactory.zoomTo(22);
+                                    mMap.moveCamera(center);
+                                    mMap.animateCamera(zoom);
+                                    if (mPositionMarker != null && mPositionMarker.isVisible() == true) {
+                                        PolylineOptions polylineOptions = new PolylineOptions();
+                                        polylineOptions.add(OldGPSPosition);
+                                        polylineOptions.addAll(RouteDeviationConvertedPoints);
+                                        Polyline polyline = mMap.addPolyline(polylineOptions);
+                                        polylineOptions.color(Color.RED).width(30);
+                                        mMap.addPolyline(polylineOptions);
+                                        polyline.setJointType(JointType.ROUND);
+                                    }
                                 }
-                          }, 10);
+
+
+                            }
+                        };
+
                     }
-              //  }
 
             }else{
 
